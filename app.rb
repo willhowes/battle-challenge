8d4695acf80c34c280e4d07bbb31fc6c4c094f6a
AnastasiiaBlaha <anastasiia.blaha@gmail.com>
2019-07-10 16:10:57 +0100

    POST/redirect/GET pattern

--- app.rb
+++ app.rb
@@ -1,16 +1,23 @@
-require 'sinatra'
+require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+enable :sessions
+
   get '/' do
-    erb(:index)
+    erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb(:play)
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
   end
-  run! if app_file == $0
+
+  get '/play' do 
+    erb :play
+  end
+
+ run! if app_file == $0
 
 end
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,14 +1,11 @@
 feature 'Enter names' do
-  # scenario 'Can run app and check page content' do
-  #   visit('/')
-  #   expect(page).to have_content 'Testing infrastructure working!'
-  # end
 
   scenario 'Players enter their names' do
     visit('/')
     fill_in :player_1_name, with: 'Will'
     fill_in :player_2_name, with: 'Anastasiia'
-    click_button('Submit')
+    click_button 'Submit'
     expect(page).to have_content 'Will vs. Anastasiia'
   end
+
 end
